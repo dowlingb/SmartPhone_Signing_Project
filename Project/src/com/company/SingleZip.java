53ed9ea98bbc20cd71e612e220cdbb31df5818ea
Brendan <bld43@case.edu>
2020-04-24 20:08:06 -0400

    1st testing round

---
+++ Project/src/com/company/SingleZip.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class SingleZip {
+    public String fileName;
+    public String extractName;
+
+    // Create a class constructor for the MyClass class
+    public SingleZip(String name) {
+        fileName = name;  // Set the initial value for the class attribute x
+    }
+
+    public boolean[] runTests()
+    {
+        return null;
+    }
+
+    public boolean extract(String extractAs)
+    {
+        return false;
+    }
+
+    public boolean extractable(String extractAs)
+    {
+        return false;
+    }
+
+    public boolean hasMF()
+    {
+        return false;
+    }
+
+    public boolean hasSF()
+    {
+        return false;
+    }
+
+    public boolean hasSignature()
+    {
+        return false;
+    }
+
+    public boolean completeMF()
+    {
+        return false;
+    }
+
+    public boolean completeSF()
+    {
+        return false;
+    }
+
+    public boolean matchSFMF()
+    {
+        return false;
+    }
+
+    public boolean matchSignatureSF()
+    {
+        return false;
+    }
+
+    public boolean difSigners()
+    {
+        return false;
+    }
+
+}
