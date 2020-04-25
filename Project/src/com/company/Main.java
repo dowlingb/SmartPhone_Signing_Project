df52e9a017e96b0b9de46c5188b40e93fcc79a4d
Brendan <bld43@case.edu>
2020-04-25 16:59:16 -0400

    Basic manifest matching

--- Project/src/com/company/Main.java
+++ Project/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         SingleZip test = new SingleZip(args[1],args[0]);
-        System.out.println(test.checkExtract());
+        //System.out.println(test.checkExtract());
     }
 
 }
--- Project/src/com/company/SingleZip.java
+++ Project/src/com/company/SingleZip.java
@@ -10,47 +10,132 @@ public class SingleZip {
     public String extractName = null;
     public boolean extracted;
     public ArrayList<String> filenames;//Contents filenames for checking
-    public String manifestFile; //MF
-    public String signatureFile; //SF
-    public String signedFile; //.rsa or whatever
+    public File manifestFile; //MF
+    public File signatureFile; //SF
+    public File signedFile; //.rsa or whatever
     public String pathZip;
+    public String extractLocation;
+    public ArrayList<String> fileNameList;//All filenames from extraction
+    public String signType;
 
     // Create a class constructor for the MyClass class
-    public SingleZip(String zipName,String pathName) {//ZIPNAME has to be the FULL path name
+    public SingleZip(String zipName, String pathName) {//ZIPNAME is simply the APK/ZIP file name and pathName is the folder path
         pathZip = pathName;
         fileNameZIP = zipName;  // Set the initial value for the class attribute x
-        unzip(pathZip+"\\" +fileNameZIP,pathZip+"\\" + fileNameZIP.substring(0,zipName.length()-4));
+        fileNameList = unzip(pathZip + "\\" + fileNameZIP, pathZip + "\\" + fileNameZIP.substring(0, zipName.length() - 4));
+        //CHECK IF ZIP EXTRACT FAILED
+
         //filenames = extract(zipName.substring(0,zipName.length()-4)+"folder");
+        extractLocation = pathZip + "\\" + fileNameZIP.substring(0, zipName.length() - 4);
+        System.out.println("SF " + grabSF());
+        System.out.println("MF " + grabMF());
 
     }
 
-    public String[] runTests()
+    public Boolean[] runTests() {
+        return null;
+    }
+
+
+    public boolean checkExtract() {
+        return extracted;
+    }
+
+    public boolean hasMF() {
+        //return new File(extractLocation + "\\" + "META-INF" + "\\" + "MANIFEST.MF").isFile();
+        return manifestFile == null;
+    }
+
+    public boolean hasSF() {
+        //return new File(extractLocation + "\\" + "META-INF" + "\\" + "MANIFEST.MF").isFile();
+        return signatureFile == null;
+    }
+
+
+    public boolean grabMF() {
+        File[] possibleFiles = new File(extractLocation + "\\" + "META-INF" + "\\").listFiles();
+        File MFFile;
+        //I want to check if the SF was duplicated or if there are multiple
+        boolean found = false;
+        for (int i = 0; i < possibleFiles.length; i++) {
+            File possible = possibleFiles[i];
+            //System.out.println(possible.getName().substring(possible.getName().length() - 3));
+            if (possible.getName().substring(possible.getName().length() - 3).equals(".MF")) {
+                if (found) {
+                    System.out.println("Duplicate MFs exist. See " + extractLocation);
+                } else {
+                    found = true;
+                    MFFile = possible;
+                    manifestFile = possible;
+                }
+
+            }
+
+        }
+
+
+        return found;
+    }
+
+
+    public boolean grabSF() {
+        File[] possibleFiles = new File(extractLocation + "\\" + "META-INF" + "\\").listFiles();
+        File SFFile;
+        //I want to check if the SF was duplicated or if there are multiple
+        boolean found = false;
+        for (int i = 0; i < possibleFiles.length; i++) {
+            File possible = possibleFiles[i];
+            //System.out.println(possible.getName().substring(possible.getName().length() - 3));
+            if (possible.getName().substring(possible.getName().length() - 3).equals(".SF")) {
+                if (found == true) {
+                    System.out.println("Duplicate SFs exist. See " + extractLocation);
+                } else {
+                    found = true;
+                    SFFile = possible;
+                    signatureFile = possible;
+                }
+
+            }
+
+        }
+
+
+        return found;
+    }
+
+    public boolean hasSignature() {
+        return false;
+    }
+
+    public boolean completeMF(String MF, ArrayList<String> allNames) //Match the MF records with the actual recovered files
+    {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(MF));
+
+            String st;
+            while ((st = br.readLine()) != null)
+                System.out.println(st);
+
+
+            return true;
+        }
+        catch(Exception e)
+
+        {
+
+            return false;
+        }
+    }
+
+    public ArrayList<String> extractNamesMF(String MF)
     {
         return null;
     }
 
 
-    public boolean checkExtract()
-    {
-        return extracted;
-    }
 
-    public boolean hasMF()
-    {
-        return false;
-    }
 
-    public boolean hasSF()
-    {
-        return false;
-    }
-
-    public boolean hasSignature()
-    {
-        return false;
-    }
-
-    public boolean completeMF()
+    public boolean correctMF()
     {
         return false;
     }
@@ -60,7 +145,7 @@ public class SingleZip {
         return false;
     }
 
-    public boolean matchSFMF()
+    public boolean matchSFMF(File MF, File SF)
     {
         return false;
     }
@@ -70,7 +155,7 @@ public class SingleZip {
         return false;
     }
 
-    public boolean difSigners(String MF, String SF)
+    public boolean difSigners(File MF, File SF)
     {
         return false;
     }
@@ -88,6 +173,7 @@ public class SingleZip {
         try
         {
             ArrayList<String> filenames = new ArrayList<String>();
+
             fis = new FileInputStream(zipFilePath);
             ZipInputStream zis = new ZipInputStream(fis);
             ZipEntry ze = zis.getNextEntry();
@@ -96,7 +182,7 @@ public class SingleZip {
                 String fileName = ze.getName();
                 filenames.add(fileName);
                 File newFile = new File(destDir + File.separator + fileName);
-                System.out.println("Unzipping to "+newFile.getAbsolutePath());
+                //System.out.println("Unzipping to "+newFile.getPath());
                 //create directories for sub directories in zip
                 new File(newFile.getParent()).mkdirs();
                 FileOutputStream fos = new FileOutputStream(newFile);
